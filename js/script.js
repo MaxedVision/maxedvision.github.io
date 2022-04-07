626dd13124a4d2d8155381518bd9e2e35032d7c9
Super <99681194+MaxedVision@users.noreply.github.com>
2022-04-07 19:33:54 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function contactCancel() {
+    document.getElementById("contact").style.display = "none";
+}
+
+function contactShow() {
+    document.getElementById("contact").style.display = "flex";
+}
